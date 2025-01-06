63aa66feb1373e381ca3b942d3dfdf8b9dcfdcfd
shohidullah <recursiveeducation@gmail.com>
2025-01-06 22:31:43 +0600

    navBar

---
+++ practiseReact/src/components/AboutMe.jsx
@@ -0,0 +1,10 @@
+export default function AboutMe() {
+  return (
+    <div>
+        <h1 >Me Shohidullah</h1>
+        <p>I am a passionate MERN stack software developer with a keen interest in solving complex problems and building efficient, scalable solutions using MongoDB, Express.js, React, and Node.js. Always eager to learn and innovate, I thrive in dynamic environments where I can collaborate, code, and create impactful applications.</p>
+    </div>
+    
+  )
+}
+
